0b2f24987ec961b9babdb192b55ecf10e0827466
deenap23 <142956234+deenap23@users.noreply.github.com>
2024-05-26 18:58:15 -0700

    fix bug

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -50,7 +50,7 @@ function initializeServiceWorker() {
     window.addEventListener("load", async () => {
       try {
         const registration = await navigator.serviceWorker.register("./sw.js", {
-          scope: "/",
+          // scope: "/",
         });
         if (registration.installing) {
           console.log("Service worker installing");

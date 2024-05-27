cd100fa583aad17a29089ef79eac6026a561205f
deenap23 <142956234+deenap23@users.noreply.github.com>
2024-05-26 18:52:47 -0700

    change path

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -49,7 +49,7 @@ function initializeServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const registration = await navigator.serviceWorker.register("/sw.js", {
+        const registration = await navigator.serviceWorker.register("./sw.js", {
           scope: "/",
         });
         if (registration.installing) {
